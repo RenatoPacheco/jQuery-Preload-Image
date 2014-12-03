b60f434809a84bf4a206d9523f934863f2fbf8e9
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-12-03 11:34:39 -0200

    O exemplo agora permite adicionar mais de uma imagem, e apagá-las também.

--- js/example.js
+++ js/example.js
@@ -1,8 +1,27 @@
-jQuery(document).ready(function ($) {
-    $('button').click(function () {
-        $('#imagem').PreloadImage({
-            'url': $('input:text').val() + '?datetime=' + (new Date()).getTime()
+var AcoesImagem = function (url) {
+    var _clone = $('.preview.model').clone().removeClass('model').appendTo($('.preview.model'));
+    $('.preview.model').after(_clone);
+    $(_clone).find('img').PreloadImage({
+        'url': url + '?datetime=' + (new Date()).getTime()
+    });
+    $(_clone).find('a').click(function (event) {
+        $(this).parent().stop().fadeOut({
+            duration: 300,
+            easing: 'swing',
+            complete: function () {
+                $(this).remove();
+            }
         });
+    });    
+};
+
+jQuery(document).ready(function ($) {
+    $('form').submit(function () {
+        AcoesImagem($('input:text').val());
+        $('input:text').val('');
         return false;
+    });
+    $('button:reset').click(function () {
+        $('.preview a').click();
     });
 });
