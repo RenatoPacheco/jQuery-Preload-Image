e76deb0a4d5605afa75e402d3e665be592b42c2b
Renato Bevilacqua Pacheco <renato2k@hotmail.com>
2014-12-03 12:38:57 -0200

    Ajustaes para navegação por tab no exemplo.

--- js/example.js
+++ js/example.js
@@ -5,23 +5,34 @@ var AcoesImagem = function (url) {
         'url': url + '?datetime=' + (new Date()).getTime()
     });
     $(_clone).find('a').click(function (event) {
-        $(this).parent().stop().fadeOut({
+        var _next = $(this).parent().next().not('.model, .trash');
+        var _prev = $(this).parent().prev().not('.model, .trash');
+        $(this).parent().addClass('trash').stop().fadeOut({
             duration: 300,
             easing: 'swing',
             complete: function () {
                 $(this).remove();
             }
         });
+        if (_next.length > 0) {
+            _next.find('a').focus();
+        } else if (_prev.length > 0) {
+            _prev.find('a').focus();
+        } else {
+            $('input:text').focus();
+        }
     });    
 };
 
 jQuery(document).ready(function ($) {
     $('form').submit(function () {
         AcoesImagem($('input:text').val());
-        $('input:text').val('');
+        $('input:text').val('').focus();
         return false;
     });
     $('button:reset').click(function () {
         $('.preview a').click();
+        $('input:text').focus();
     });
+    $('input:text').focus();
 });
